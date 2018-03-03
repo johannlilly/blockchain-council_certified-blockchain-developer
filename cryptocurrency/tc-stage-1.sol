5e9332a8e2643b53af79aad0ef4f5fa7c5327170
jlilly <johannlilly@users.noreply.github.com>
2018-03-03 23:39:58 +0100

    Create function transfer()
    Deduct value to send from the balance of the sender. Add the value being sent to the receiver's account.
    NOTE: we have not used any checks before transferring. This is the minimum-viable contract.

--- cryptocurrency/tc-stage-1.sol
+++ cryptocurrency/tc-stage-1.sol
@@ -9,4 +9,14 @@ contract TCoin {
 		// the contract begins by creating/adding the initial supply to the sender's account
 		balanceOf[msg.sender] = initialSupply;
 	}
+
+	// takes 2 args: the adress that you want to send to, and the amount you want to transfer
+	function transfer(address _to, uint256 _value) {
+		// deduct value to send from the balance of the sender
+		balanceOf[msg.sender] -= _value;
+		// add the value being sent to the receiver's account
+		balanceOf[_to] += _value;
+		// NOTE: we have not used any checks before transferring.
+		// this is the minimum-viable contract
+	}
 }

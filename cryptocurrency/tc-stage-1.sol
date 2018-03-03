818224ececc7d1ba9c9d710db0b5973ad5f982bb
jlilly <johannlilly@users.noreply.github.com>
2018-03-03 23:33:31 +0100

    Create constructor
    the contract begins by creating/adding the initial supply to the sender's account

--- cryptocurrency/tc-stage-1.sol
+++ cryptocurrency/tc-stage-1.sol
@@ -3,4 +3,10 @@ contract TCoin {
 	mapping (address => uint256) public balanceOf; // show the balance, which is stored as uint256
 	// whenever you want to access the balance of an address
 	// balanceOf[address] = __
+
+	// create constructor
+	function TCoin(uint256 initialSupply) {
+		// the contract begins by creating/adding the initial supply to the sender's account
+		balanceOf[msg.sender] = initialSupply;
+	}
 }

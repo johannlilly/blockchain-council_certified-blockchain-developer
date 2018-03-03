450ca279a77c28a053f2aec46477bacf7649b0b8
jlilly <johannlilly@users.noreply.github.com>
2018-03-03 23:33:15 +0100

    Configure mapping

--- cryptocurrency/tc-stage-1.sol
+++ cryptocurrency/tc-stage-1.sol
@@ -1,4 +1,6 @@
 pragma solidity 0.4.8;
 contract TCoin {
-
+	mapping (address => uint256) public balanceOf; // show the balance, which is stored as uint256
+	// whenever you want to access the balance of an address
+	// balanceOf[address] = __
 }

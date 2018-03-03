3c23dded06ecd8444ed58a9b77cb53170aef7263
jlilly <johannlilly@users.noreply.github.com>
2018-03-03 23:11:16 +0100

    Understanding the parts of a smart contract
    version, import, contract name, state variables, events, function modifiers, structs, constructor, functions

--- solidity/layout.sol
+++ solidity/layout.sol
@@ -1,11 +1,15 @@
 //versiona pragma
+// maximum version number for which this code can be compiled
+// you may also see this => ^0.4.8 ; which indicates that version and above cannot compile
 pragma solidity 0.4.8; 
 
 //import section
+// import an external file or external smart contract from another location. It should have a .sol extension
 import "filename";
 
 //begin the contract
 /// @title This is the layout of the solidity code
+// everything needs to be within the contract block
 contract ContractName {
     /*
     * @title A Simple Layout Example
@@ -15,7 +19,7 @@ contract ContractName {
     * 
     */
 
-    // This is a single-line comment.
+     // This is a single-line comment.
 
     /*
     This is a
@@ -23,17 +27,25 @@ contract ContractName {
     */
 
     // State Variables
+    // variables can be anywhere in the contract, even below the function modifiers
+    // address, uint, string = data type
+    // public, private = access type
+    // stateVariable1, stateVariable2 = variable name
     address public stateVariable1;
     uint public stateVariable2;
     uint private stateVariable3;
     string public constant HOST_ID = 0x1234;
 
     // Events
+    // events are optional
+    // events are ways to log the particular values in the interface
+    // this is also how you perform debugging
     event LogEvent1(address param1, uint param2);
     event LogEvent2(address param1);
     event LogEvent3();
 
     // Function Modifiers
+    // modifiers are the way to set restrictions on certain functions
     modifier onlyIfOwnerModifier() { 
         if (msg.sender != owner) throw;
         _;
@@ -45,6 +57,7 @@ contract ContractName {
     }
 
     // Struct, arrays or Enum if any here
+    // here, you are simply defining data types
     enum enum1 { val1, val2, val3 }
     struct struct1 { 
         uint weight;
@@ -55,6 +68,8 @@ contract ContractName {
 
 
     // Define consutruct here
+    // the constructor of the smart contract
+    // MUST have the same name as the contract name
     function ContractName(uint initialCoins) {
        // Initialize state variables here
     }
@@ -78,6 +93,7 @@ contract ContractName {
     }
 
     //default function
+    // define a default function to execute if there is no particular function for defined for the function that you are calling
     function(){
         throw;
     }
